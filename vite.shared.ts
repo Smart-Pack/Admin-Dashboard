15e6fd09c771784f6d69e0874bfb27e890e0223b
Trevor Gituru <giturutrevor@gmail.com>
2026-09-09 14:35:53 +0300

    chore(config): configure local development server

---
+++ vite.shared.ts
@@ -0,0 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export const sharedConfig = {
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+}
