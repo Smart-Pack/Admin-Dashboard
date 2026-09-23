aa7ec9508ce6f08fb3a675e6542a2c865d364f46
Trevor Gituru <giturutrevor@gmail.com>
2026-09-23 11:17:05 +0300

    fix: update Vue global property typings
    Type global Vue properties from their registered implementations and add typings for filters and notification helpers.

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,7 +1,21 @@
-import type { Api } from '@/api/types'
+import type { SweetAlertOptions } from 'sweetalert2'
+import api from '@/api'
+import Filters from '@/helpers/filters'
 
 declare module 'vue' {
   interface ComponentCustomProperties {
-    $api: Api
+    $api: typeof api
+
+    $filters: typeof Filters
+
+    $notifySuccess: (message: string, overrides?: SweetAlertOptions) => Promise<unknown>
+
+    $notifyError: (message: string, overrides?: SweetAlertOptions) => Promise<unknown>
+
+    $deleteModal: (
+      action: string,
+      message: string,
+      overrides?: SweetAlertOptions,
+    ) => Promise<boolean>
   }
 }
