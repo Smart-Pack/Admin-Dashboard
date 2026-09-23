f72218929f831eaa6b58d5b34ebdabe62552a21b
Trevor Gituru <giturutrevor@gmail.com>
2026-09-23 11:18:39 +0300

    feat: add table footer pagination
    Add a reusable table footer with pagination controls, page-size selection, navigation icons, and unit tests.

---
+++ src/components/Icons/RightIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="size-6"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+  </svg>
+</template>
+
+<script lang="ts">
+/**
+ * @module components/Icons/RightIcon
+ * @description SVG icon component for next.
+ */
+export default {
+  name: 'RightIcon',
+}
+</script>
